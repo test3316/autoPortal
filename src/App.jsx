54655e425e8fffaaeb35c72774b04dcb33f2de53
Giorgi Kochiashvili <qochoo@gmail.com>
2023-12-04 22:16:33 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="border-8">Welcome!!!</h1>
+      <h1 className="">Welcome</h1>
+    </>
+  );
+}
+
+export default App;
